be7b52e605bbf5e194bc3bb064a1f7e961b66a3e
arques89 <arquesjavier@gmail.com>
2022-05-25 10:07:39 +0000

    ajustado %

--- src/js/component/semaforo.jsx
+++ src/js/component/semaforo.jsx
@@ -5,31 +5,18 @@ export const TrafficLight = () => {
 
 	return (
 		<div>
-			<div className="semaforo"></div>
 			<div className="container">
-				<div className="sem">
-					<div
-						className={
-							"light red " + (light === "red" ? "glow" : "")
-						}
-						onClick={() => setlight("red")}></div>
-					<div
-						className={
-							"light orange " + (light === "orange" ? "glow" : "")
-						}
-						onClick={() => setlight("orange")}></div>
-					<div
-						className={
-							"light green " + (light === "green" ? "glow" : "")
-						}
-						onClick={() => setlight("green")}></div>
-				</div>
+				<div
+					className={"red " + (light === "red" ? "glow" : "")}
+					onClick={() => setlight("red")}></div>
+				<div
+					className={"orange " + (light === "orange" ? "glow" : "")}
+					onClick={() => setlight("orange")}></div>
+				<div
+					className={"green " + (light === "green" ? "glow" : "")}
+					onClick={() => setlight("green")}></div>
 			</div>
-			<div className="poste">
-				<div className="señal">
-					<div className="señal1">Prohibido</div>
-				</div>
-			</div>
+			<div className="poste"></div>
 		</div>
 	);
 };
